11702ee8d8e16abd25710a6b2b8d98745845a292
frailyng <frailindes@gmail.com>
2025-05-16 23:04:56 -0400

    Main modificado para los tickets

--- app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/MainActivity.kt
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/MainActivity.kt
@@ -14,11 +14,16 @@ import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.database.Tecn
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.presentation.tecnicos.TecnicosViewModel
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.repository.TecnicosRepository
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.presentation.navigation.TecnicosNavHost
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.presentation.navigation.TicketNavHost
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.presentation.tickets.TicketViewModel
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.repository.TicketsRepository
 
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
     private lateinit var tecnicosRepository: TecnicosRepository
+    private lateinit var ticketsRepository: TicketsRepository
     private lateinit var tecnicosViewModel: TecnicosViewModel
+    private lateinit var ticketViewModel: TicketViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,12 +37,20 @@ class MainActivity : ComponentActivity() {
             .build()
 
         tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
+        ticketsRepository = TicketsRepository(tecnicoDb.TicketDao())
 
         tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
+        ticketViewModel = TicketViewModel(ticketsRepository)
 
         setContent {
             val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
             val tecnicoList by tecnicoDb.TecnicoDao().getAll()
+                .collectAsStateWithLifecycle(
+                    initialValue = emptyList(),
+                    lifecycleOwner = lifecycleOwner,
+                    minActiveState = Lifecycle.State.STARTED
+                )
+            val ticketList by tecnicoDb.TicketDao().getAll()
                 .collectAsStateWithLifecycle(
                     initialValue = emptyList(),
                     lifecycleOwner = lifecycleOwner,
@@ -49,6 +62,12 @@ class MainActivity : ComponentActivity() {
                     nav,
                     tecnicoList,
                     tecnicosViewModel,
+                    nav
+                )
+                TicketNavHost(
+                    nav,
+                    ticketList,
+                    ticketViewModel,
                     nav
                 )
             }
