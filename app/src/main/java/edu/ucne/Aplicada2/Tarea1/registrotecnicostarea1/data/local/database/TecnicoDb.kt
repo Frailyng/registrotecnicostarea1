ad06745c8cff78ee1fab55a5d6d065b136e5f24b
frailyng <frailindes@gmail.com>
2025-05-16 22:18:06 -0400

    Base de datos modificada para los tickets

--- app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/database/TecnicoDb.kt
@@ -3,15 +3,19 @@ package edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.TecnicoDao
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.TicketDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.TecnicoEntity
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.TicketEntity
 
 @Database(
     entities = [
-        TecnicoEntity::class
+        TecnicoEntity::class,
+        TicketEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
+    abstract fun TicketDao(): TicketDao
 }
