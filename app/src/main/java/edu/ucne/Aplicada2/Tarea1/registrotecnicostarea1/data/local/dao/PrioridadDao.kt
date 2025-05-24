97122515020a3c8c5975b0cc763baeac44ad90f1
frailyng <frailindes@gmail.com>
2025-05-24 11:04:38 -0400

    PrioridadDao creada

--- app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/dao/PrioridadDao.kt
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/dao/PrioridadDao.kt
@@ -1,4 +1,31 @@
 package edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.Prioridades
+import kotlinx.coroutines.flow.Flow
+
 interface PrioridadDao {
+
+    @Upsert
+    suspend fun save(prioridad: Prioridades)
+
+    @Query(
+        """
+            SELECT *
+            FROM Prioridades
+            WHERE prioridadId=:id
+            LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): Prioridades?
+
+    @Delete
+    suspend fun delete(prioridad: Prioridades)
+
+    @Query("SELECT * FROM Prioridades")
+    fun getAll(): Flow<List<Prioridades>>
+
 }
