daf256195014ef2c440d137390ff7d059c68fb82
frailyng <frailindes@gmail.com>
2025-05-24 11:10:40 -0400

    PrioridadadesRepository creado

---
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/repository/PrioridadadesRepository.kt
@@ -0,0 +1,15 @@
+package edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.repository
+
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.PrioridadDao
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.Prioridades
+import kotlinx.coroutines.flow.Flow
+
+class PrioridadadesRepository(
+    private val dao: PrioridadDao
+){
+    suspend fun save(prioridad: Prioridades) = dao.save(prioridad)
+    suspend fun find(id: Int): Prioridades? = dao.find(id)
+    suspend fun delete(prioridad: Prioridades) = dao.delete(prioridad)
+
+    fun getAll(): Flow<List<Prioridades>> = dao.getAll()
+}
