265ded776d6240cb6a3617e7c39e3eb5cc750f36
frailyng <frailindes@gmail.com>
2025-05-24 11:07:09 -0400

    Tecnicodb Modificado para prioridades

--- app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/database/TecnicoDb.kt
@@ -2,15 +2,18 @@ package edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.PrioridadDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.TecnicoDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.TicketDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.TecnicoEntity
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.TicketEntity
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.Prioridades
 
 @Database(
     entities = [
         TecnicoEntity::class,
-        TicketEntity::class
+        TicketEntity::class,
+        Prioridades::class
     ],
     version = 3,
     exportSchema = false
@@ -18,4 +21,5 @@ import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.Tick
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
     abstract fun TicketDao(): TicketDao
+    abstract fun PrioridadDao(): PrioridadDao
 }
