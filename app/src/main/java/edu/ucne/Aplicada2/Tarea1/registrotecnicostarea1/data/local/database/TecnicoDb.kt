a5197c903d469feab3901b8e4753f8c4949c1046
frailyng <frailindes@gmail.com>
2025-05-25 12:36:22 -0400

    Database modificado para inyectar a mensaje

--- app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/local/database/TecnicoDb.kt
@@ -4,9 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.MensajeDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.PrioridadDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.TecnicoDao
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.dao.TicketDao
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.MensajeEntity
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.TecnicoEntity
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.TicketEntity
 import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.local.entities.Prioridades
@@ -28,7 +30,8 @@ class Converters {
     entities = [
         TecnicoEntity::class,
         TicketEntity::class,
-        Prioridades::class
+        Prioridades::class,
+        MensajeEntity::class
     ],
     version = 5,
     exportSchema = false
@@ -38,4 +41,5 @@ abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
     abstract fun TicketDao(): TicketDao
     abstract fun PrioridadDao(): PrioridadDao
+    abstract fun MensajeDao(): MensajeDao
 }
