d313299fca00d61f539da1397709f924d5ad5c2e
frailyng <frailindes@gmail.com>
2025-06-14 18:23:09 -0400

    RemoteDataSource modificado con los metodos correspondientes para acceder a lo datos de la Api

--- app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/Aplicada2/Tarea1/registrotecnicostarea1/data/remote/RemoteDataSource.kt
@@ -1,1 +1,18 @@
 package edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.remote
+
+import edu.ucne.Aplicada2.Tarea1.registrotecnicostarea1.data.remote.dto.CompraDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val compraApi: CompraApi
+){
+    suspend fun getCompras()= compraApi.getCompras()
+
+    suspend fun updateCompra(compraDto: CompraDto)= compraApi.updateCompra(compraDto)
+
+    suspend fun saveCompra(compraDto: CompraDto)= compraApi.saveCompra(compraDto)
+
+    suspend fun  deleteCompra(id: Int)= compraApi.deleteCompra(id)
+
+    suspend fun getCompra(id: Int)= compraApi.getCompras(id)
+}
